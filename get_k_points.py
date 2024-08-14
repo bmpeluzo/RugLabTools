e11ddeb3dead2d85df0136240e2e12391ed8a0ea
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-08-14 12:20:18 -0400

    "lalala"

--- get_k_points.py
+++ get_k_points.py
@@ -1,6 +1,6 @@
 from cry_input import build_k
 
 dropbox_dir='/home/bmpeluzo/Dropbox/Rochester/Research/CIF/'
-cif='BTBT'
+cif='NAPSEZ10'
 cif_path=dropbox_dir+cif+'.cif'
 build_k(cif_file=cif_file,max_dens=20)
