c6b5281bbfa341a70fb8b454881dc69c0d04b34d
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-08-14 11:20:18 -0400

    Updated file

--- get_k_points.py
+++ get_k_points.py
@@ -1,4 +1,6 @@
-from k_points import build_k
+from cry_input import build_k
 
-cif_file='/home/bmpeluzo/Dropbox/Rochester/Research/CIF/BTBT.cif'
+dropbox_dir='/home/bmpeluzo/Dropbox/Rochester/Research/CIF/'
+cif='BTBT'
+cif_path=dropbox_dir+cif+'.cif'
 build_k(cif_file=cif_file,max_dens=20)
