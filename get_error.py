36874974072b3faec69d1632f43928cdb43d5614
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-09-05 12:35:26 -0400

    Works well

--- get_error.py
+++ get_error.py
@@ -24,31 +24,64 @@ def file_io(file):
 
 ### getting experimental parameters
 
-for i in range(len(file_io(sys.argv[1]))):
-    line_a=file_io(sys.argv[1])[i].find("_cell_length_a")
+cif=file_io(sys.argv[1])
+for i in range(len(cif)):
+    line_a=cif[i].find("_cell_length_a")
     if line_a!=-1:
-        a_exp=file_io(sys.argv[1])[i].split()[len(file_io(sys.argv[1])[i].split())-1]
+        a_exp=cif[i].split()[len(cif[i].split())-1]
         a_exp=ufloat_fromstr(a_exp).n
-        b_exp=file_io(sys.argv[1])[i+1].split()[len(file_io(sys.argv[1])[i].split())-1]
+        b_exp=cif[i+1].split()[len(cif[i].split())-1]
         b_exp=ufloat_fromstr(b_exp).n
-        c_exp=file_io(sys.argv[1])[i+2].split()[len(file_io(sys.argv[1])[i].split())-1]
+        c_exp=cif[i+2].split()[len(cif[i].split())-1]
         c_exp=ufloat_fromstr(c_exp).n
-        alpha_exp=file_io(sys.argv[1])[i+3].split()[len(file_io(sys.argv[1])[i].split())-1]
+        alpha_exp=cif[i+3].split()[len(cif[i].split())-1]
         alpha_exp=int(alpha_exp)
-        beta_exp=file_io(sys.argv[1])[i+4].split()[len(file_io(sys.argv[1])[i].split())-1]
+        beta_exp=cif[i+4].split()[len(cif[i].split())-1]
         beta_exp=int(beta_exp)
-        gamma_exp=file_io(sys.argv[1])[i+5].split()[len(file_io(sys.argv[1])[i].split())-1]
+        gamma_exp=cif[i+5].split()[len(cif[i].split())-1]
         gamma_exp=int(gamma_exp)
         break
 
+
+
 ######### getting the experimental ##############
 
-for i in range(len(file_io(sys.argv[2]))):
-    opt_geom=file_io(sys.argv[2])[i].find("FINAL OPTIMIZED GEOMETRY")
+cry_out=file_io(sys.argv[2])
+
+for i in reversed(range(len(cry_out)-1)):
+    opt_geom=cry_out[i].find("FINAL OPTIMIZED GEOMETRY")
     if opt_geom!=-1:
-        print(file_io(sys.argv[2])[i])
-        for j in range(i,len(file_io(sys.argv[2]))):
-            cell=file_io(sys.argv[2])[j].find("CRYSTALLOGRAPHIC CELL")
-            print(file_io(sys.argv[2])[j])
-            if cell!=-1:
-                print(file_io(sys.argv[2])[j+2])
+        opt_line=i
+        break
+
+for j in range(i,len(cry_out)):
+    cell=cry_out[j].find("CRYSTALLOGRAPHIC CELL (VOLUME=") ## try to find crystallographic cell
+    if cell!=-1:
+        line=j+2
+        break
+
+try:
+    line
+except NameError:
+    for j in range(i,len(cry_out)): ## if not, cell parameters are the one from primitive cell
+        cell=cry_out[j].find("PRIMITIVE CELL")
+        if cell!=-1:
+            line=j+2
+            break
+
+a_theo=float(cry_out[line].split()[0])
+b_theo=float(cry_out[line].split()[1])
+c_theo=float(cry_out[line].split()[2])
+alpha_theo=float(cry_out[line].split()[3])
+beta_theo=float(cry_out[line].split()[4])
+gamma_theo=float(cry_out[line].split()[5])
+
+
+####### calculate errors ###########
+
+def get_err(theo,exp):
+    err=((theo-exp)/exp)*100
+    return err
+
+print("Erros:\n err = (theo-exp)/exp * 100")
+print(get_err(a_theo,a_exp),get_err(b_theo,b_exp),get_err(c_theo,c_exp),get_err(alpha_theo,alpha_exp),get_err(beta_theo,beta_exp),get_err(gamma_theo,gamma_exp))
