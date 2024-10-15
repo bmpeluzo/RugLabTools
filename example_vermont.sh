ba039f2ca155426b83b906361e3603fe0731dc2f
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-10-15 12:20:02 -0400

    It works for geom opt, copies only essential files

--- example_vermont.sh
+++ example_vermont.sh
@@ -3,7 +3,8 @@
 
 #### USER INPUT #####
 job_name=example
-input_name=false # True if you call your input something.d12
+input_name=true # True if you call your input something.d12
+
 
 ## Path to binary on Bluehive 
 binary=/scratch/mruggie8_lab/software/CRYSTAL/CRYSTAL23_BARBARA/bin/bluehive_impi_nodebug/std/Pcrystal
@@ -12,6 +13,7 @@ binary=/scratch/mruggie8_lab/software/CRYSTAL/CRYSTAL23_BARBARA/bin/bluehive_imp
 
 ###### CHECK IF THE JOB WAS SUBMITTED FROM HOME OR SCRATCH ####
 sub_dir=$(pwd | cut -d/ -f2)
+job_dir=$PWD
 
 #set -x  #echo on
 module load impi/2020.1 #Load the intel library (already exists on compute nodes)
@@ -23,10 +25,8 @@ jobdir=/home/$USER/$rand
 #jobdir=/home/$USER/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
 remotebinary=$jobdir/Pcrystal
 
-echo $rand
-
 # copy input file to INPUT (in case your input is already called INPUT, comment out this line)
-if [ ${input_name} = true ]; then
+if [ "${input_name}" = true ]; then
 	cp ${job_name}.d12 INPUT
 fi
 
@@ -54,22 +54,66 @@ mpirun -ppn 24 -hosts $hostlist -wdir=$jobdir $remotebinary </dev/null &> $job_n
 cp ${job_name}.out ${job_name}_$rand.out
 
 # Copy files back to bluehive and remove jobdir from remote host
+
 if [[ $sub_dir == "home" ]]; then
 	out_dir=$SCRATCH/${job_name}_$rand
 	echo "copying output to scratch"
 elif [[ $sub_dir == "scratch" ]]; then
-	out_dir=${job_name}_$rand
+	out_dir=${PWD}/${job_name}_$rand
 else
 	out_dir=$SCRATCH/${job_name}_$rand
 	echo "Unable to detect submission directory. Output files are located at $out_dir"
 fi
 
-
 mkdir -p ${out_dir}
-cd ${out_dir}/
+mkdir -p ${out_dir}_tmp/
 
 for host in $@; do
-
-scp -r $host:$jobdir ./$host && ssh $host "rm -rf $jobdir"
+scp -r $host:$jobdir/* ${out_dir}_tmp/ && ssh $host "rm -rf $jobdir"
 done
+
+######### Keeping just essential files #######
+out_files=(KAPPA.DAT POWER_C.DAT POWER.DAT SEEBECK.DAT SIGMA.DAT SIGMAS.DAT TDF.DAT OPTHESS.DAT OPTINFO.DAT XMETRO.COR BORN.DAT IRREFR.DAT IRDIEL.DAT ADP.DAT ELASINFO.DAT EOSINFO.DAT FINDSYM.DAT FREQINFO.DAT TENS_IR.DAT IRSPEC.DAT TENS_RAMAN.DAT RAMSPEC.DAT SCANPES.DAT VIBPOT.DAT MOLDRAW.DAT PCF.DAT INTEGRATED_PCF.DAT INTERPOL_PCF.DAT SCFOUT.LOG FREQUENCIES.DAT FREQPLOT.DAT HESSFREQ.DAT)
+
+out_files2=(fort.34 fort.9 fort.20 fort.69 fort.98 fort.80 fort.13 fort.85 fort.90 fort.21 fort.75 fort.28)
+
+cd ${out_dir}_tmp/
+for file in $(ls); do
+	if [[ " ${out_files[*]} " =~ [[:space:]]${file}[[:space:]] ]]; then
+		if [ "${input_name}" = true ]; then
+			file_name=${job_name}.${file}
+		else
+			file_name=${file}
+		fi
+	cp ${file} ${out_dir}/${file_name}
+	fi
+	if [[ " ${out_files2[*]} " =~ [[:space:]]${file}[[:space:]] ]]; then
+		if [ "${input_name}" = true ]; then
+			file_name=${job_name}.f$(ls $file | cut -d. -f2)
+		else
+			file_name=${file}
+		fi
+	cp ${file} ${out_dir}/${file_name}
+	fi
+	if [ "$file" = "GAUSSIAN.DAT" ]; then
+		if [ "${input_name}" = true ]; then
+			file_name=${job_name}.gjf
+		else
+			file_name=${file}
+		fi
+	cp ${file} ${out_dir}/${file_name}
+	fi
+	if [ "$file" = "GEOMETRY.CIF" ]; then
+		if [ "${input_name}" = true ]; then
+			file_name=${job_name}.cif
+		else
+			file_name=${file}
+		fi
+	cp ${file} ${out_dir}/${file_name}
+	fi
+done
+cd ../
+
+mv ${job_dir}/${job_name}_$rand.out ${out_dir}/
+rm -r ${out_dir}_tmp/
 
